78bcb5b5b32fab0976c05f676d949cbaa8d712d0
Ashish <Ashishking2014@gmail.com>
2023-01-14 23:05:10 +0530

    Initial Project upload

---
+++ lib/module/camera_view.dart
@@ -0,0 +1,34 @@
+import 'package:camera/camera.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:google_mlkit_text_recognition/google_mlkit_text_recognition.dart';
+
+import '../main.dart';
+
+class CameraView extends StatefulWidget {
+  const CameraView(
+      {Key? key,
+        required this.title,
+        required this.customPaint,
+        this.text,
+        required this.onImage,
+        this.initialDirection = CameraLensDirection.back})
+      : super(key: key);
+
+  final String title;
+  final CustomPaint? customPaint;
+  final String? text;
+  final Function(InputImage inputImage) onImage;
+  final CameraLensDirection initialDirection;
+
+  @override
+  State<CameraView> createState() => _CameraViewState();
+}
+
+class _CameraViewState extends State<CameraView> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+
+}
